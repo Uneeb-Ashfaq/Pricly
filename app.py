bdad2d7574ede1caf53e6c8b4bc6ddf78a544b42
Uneeb-Ashfaq <uneebash24@gmail.com>
2025-12-27 00:13:03 -0500

    set up  SQLAlchemy

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template,url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URL'] = 'sqlite:///'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    date_added = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+            return f'<Product {self.name}>'
 
 @app.route('/')
 
 def index():
     return render_template('index.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
